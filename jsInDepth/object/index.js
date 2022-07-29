c26f437029a7b9a002a722afb7e0c853cf7dde59
knightch <amankumarpandey2397@gmail.com>
2022-07-29 23:55:36 +0530

    declare object and assigning values

---
+++ jsInDepth/object/index.js
@@ -0,0 +1,10 @@
+// define objects
+let obj = {} // first way
+let obj1 = new Object() // second way
+
+// assigning values in objects
+obj = {
+    name: 'Aman'
+}
+obj1.name = 'Raju';
+obj1['age'] = 25;
