d3be4a15c14f50925421968eca3698da26c1b0a4
knightch <amankumarpandey2397@gmail.com>
2022-07-30 00:56:41 +0530

    understand outer env

--- jsInDepth/object/index.js
+++ jsInDepth/object/index.js
@@ -15,23 +15,28 @@ console.log('this', this)
 console.log(window === this)
 
 // when we initailize a value or function it goes to global scope that is goes to window object
-var a = 'aman'
+var a = 'aman' // hoisting phase
 
 function b() {
     console.log('my name is raju also!')
-}
+} // hoisting phase
 
 // global excution context
-console.log(a)
+console.log(a) // excution phase
 // functional excution context
-b();
+b(); // excution phase
 
 //  excution stack 
 var color = "red";
 
 function chengeColor() {
     var color = "green";
-    console.log(`change color is ${color}`)
+    console.log(`change color is ${color}`);
+    newChanges();
+}
+
+function newChanges() {
+    console.log(color); // calling outer env
 }
 chengeColor();
 console.log(`initail color is ${color}`)
